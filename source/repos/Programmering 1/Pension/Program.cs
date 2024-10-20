3ba8a8c89802d4d1a17d3a78dc23d2dacd413fdd
Rayw1n <Alexander.Firmager@hotmail.com>
2024-10-20 19:43:04 +0200

    Programmering 1

---
+++ source/repos/Programmering 1/Pension/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Hej och välkommen till denna pensionssimulator!");
+
+// Ber användaren om dess namn.
+Console.WriteLine("Vad heter du?");
+string namn = Console.ReadLine();
+
+// Frågar hur gammal användaren är.
+Console.WriteLine("Hur gammal är du?");
+int old = int.Parse(Console.ReadLine());
+
+int pension = 67;
+int leftPension = pension - old;
+
+//Räknar ut pensionen utifrån sagd ålder.
+if (leftPension > 0)
+{
+    Console.WriteLine($"{namn} du har {leftPension} år kvar till pension.");
+}
+else
+{
+    Console.WriteLine($"{namn}, Du är tillräckligt gammal för att pensionera dig, eller så är du redan pensionär.");
+}
+Console.WriteLine("Tryck på valfri knapp för att avsluta...");
+Console.ReadLine();
